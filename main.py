661807eae1f0fa0bf47e62aa0e50a2468beea393
ChordShore <contact@chordshore.com>
2025-05-20 12:36:40 -0700

    Added framerate limit to 60 FPS and circle object class

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init
+    clock = pygame.time.Clock()
+    delta_time = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     number = 0
@@ -15,6 +17,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        delta_time = (clock.tick(60))/1000
         pygame.display.flip()
 
 if __name__ == "__main__":
